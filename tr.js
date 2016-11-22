4d13ad7d49f67c7d68a580d2db5dc86815ab7ca4
Nugroho Gito <nugroho.gito@yahoo.com>
2016-11-22 22:11:33 +0700

    Initial Version

---
+++ tr.js
@@ -0,0 +1,19 @@
+// ----------------------------------------------------------------------------
+// Sample code using Google Translate API
+// ----------------------------------------------------------------------------
+
+require( 'dotenv' ).config( {silent: true} );
+
+var translateApiKey = process.env.TRANSLATE_API_KEY
+var googleTranslate = require('google-translate')(translateApiKey);
+var languageTo = "en";
+
+process.stdout.write("Enter any text to be translated to " + languageTo + ", Ctrl-C to exit\n");
+process.stdin.pipe(require('split')()).on('data', processLine)
+
+function processLine (line) {
+  googleTranslate.translate(line, languageTo, function(err, translation) {
+    console.log(translation.translatedText);
+});
+}
+
